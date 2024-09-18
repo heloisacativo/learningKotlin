7c786bad6f05bf6a0b6ab413712bc6e87d7473d0
heloisacativo <heloisacativo@gmail.com>
2024-09-18 19:51:46 -0300

    add examples floating types

---
+++ app/src/main/java/com/course/learningkotlinfundamentalsday_1_2_3/ui/theme/FloatingTypes.kt
@@ -0,0 +1,15 @@
+package com.course.learningkotlinfundamentalsday_1_2_3.ui.theme
+
+fun main() {
+    var circleRatio: Float = 3.14f
+    println(circleRatio)
+
+    circleRatio = 3.4567f
+    println(circleRatio)
+
+    val floatNumber: Float = 3.14159265358979323846f
+    val doubleNumber: Double = 3.14159265358979323846
+
+    println("Float: $floatNumber")   // Saída: Float: 3.1415927 (apenas os 7 primeiros dígitos são precisos)
+    println("Double: $doubleNumber") // Saída: Double: 3.141592653589793 (mais dígitos são precisos)
+}
