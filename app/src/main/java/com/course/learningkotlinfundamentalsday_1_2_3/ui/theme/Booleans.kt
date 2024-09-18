a4ee61291d543d32d1150d88a3d6c1fc2783dbae
heloisacativo <heloisacativo@gmail.com>
2024-09-18 20:09:20 -0300

    add booleans example

---
+++ app/src/main/java/com/course/learningkotlinfundamentalsday_1_2_3/ui/theme/Booleans.kt
@@ -0,0 +1,10 @@
+package com.course.learningkotlinfundamentalsday_1_2_3.ui.theme
+
+fun main(){
+    var myTrue = true
+    var myFalse = false
+
+    println(myTrue || myFalse) // Saída: true, por que || é um operador lógico(ou), se pelo menos um for true, o resultado é true, se ambos forem false, o resultado é false,
+    println(myTrue && myFalse) // Saída: false, por que && é um operador lógico(e), se ambos forem true, o resultado é true
+    println(!myTrue) // Saída: false, por que ! é um operador lógico(não), se o valor é true, ele vira false, se o valor é false, vira true, no caso myTrue é true então o resultado é false.
+}
