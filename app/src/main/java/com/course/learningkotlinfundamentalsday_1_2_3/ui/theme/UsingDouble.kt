d047c1a32ec59490c7294cb4a0294513d0f3e30d
heloisacativo <heloisacativo@gmail.com>
2024-09-18 20:55:19 -0300

    add double input example

---
+++ app/src/main/java/com/course/learningkotlinfundamentalsday_1_2_3/ui/theme/UsingDouble.kt
@@ -0,0 +1,18 @@
+package com.course.learningkotlinfundamentalsday_1_2_3.ui.theme
+
+fun main(){
+    println("Enter number 1")
+    val num1Srt = readln()
+    val num1D = num1Srt.toDouble()
+    println("Enter number 2")
+    val num2Str = readln()
+    val num2D = num2Str.toDouble()
+
+    val myResult = divide(num1D, num2D)
+    println(myResult)
+}
+
+fun divide(num1: Double, num2: Double):Double{
+    val result = num1/num2
+    return result
+}
