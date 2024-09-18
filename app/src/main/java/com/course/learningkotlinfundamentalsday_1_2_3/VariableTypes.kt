ededa79784f9f342ece513e406f6e724354d917a
heloisacativo <heloisacativo@gmail.com>
2024-09-18 18:54:51 -0300

    variables val and var

---
+++ app/src/main/java/com/course/learningkotlinfundamentalsday_1_2_3/VariableTypes.kt
@@ -0,0 +1,11 @@
+package com.course.learningkotlinfundamentalsday_1_2_3
+
+fun main(){
+    val constantNumber = 20 // val não pode ser alterado o valor da variável
+    var mutableNumber = 10 // var pode ser alterado
+
+    println(constantNumber)
+    println(mutableNumber)
+    mutableNumber = 3;
+    println(mutableNumber)
+}
