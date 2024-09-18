09cf8aaafb00d46111875cc39394286872ab8cd3
heloisacativo <heloisacativo@gmail.com>
2024-09-18 20:52:25 -0300

    add input int example

---
+++ app/src/main/java/com/course/learningkotlinfundamentalsday_1_2_3/ui/theme/InputTwoNumbers.kt
@@ -0,0 +1,13 @@
+package com.course.learningkotlinfundamentalsday_1_2_3.ui.theme
+
+fun main(){
+    println("Enter number 1")
+    val num1Str = readln()
+    val num1Int = num1Str.toInt()
+    println("Enter number 2")
+    val num2Str = readln()
+    val num2Int = num2Str.toInt()
+
+    val myResult = add(num1Int, num2Int)
+    println(myResult)
+}
