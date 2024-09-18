6f7c3d8b81522bfe17847314b9608c69f09108ba
heloisacativo <heloisacativo@gmail.com>
2024-09-18 19:11:36 -0300

    add example while

---
+++ app/src/main/java/com/course/learningkotlinfundamentalsday_1_2_3/While.kt
@@ -0,0 +1,10 @@
+package com.course.learningkotlinfundamentalsday_1_2_3
+
+fun main() {
+    var userInput = readln()
+    while (userInput == "1") {
+        println("While loop executed")
+        userInput = readln();
+    }
+
+}
