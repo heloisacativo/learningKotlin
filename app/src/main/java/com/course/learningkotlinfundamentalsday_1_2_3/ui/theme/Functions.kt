763cf968520d330ec015626aeb17098ef7010c19
heloisacativo <heloisacativo@gmail.com>
2024-09-18 19:57:20 -0300

    add functions example

---
+++ app/src/main/java/com/course/learningkotlinfundamentalsday_1_2_3/ui/theme/Functions.kt
@@ -0,0 +1,12 @@
+package com.course.learningkotlinfundamentalsday_1_2_3.ui.theme
+
+fun main(){
+    val myResult = add(5,5)
+    println(myResult)
+}
+
+// o :Int após o parâmetro ele indica que essa função irá retornar um valor do tipo Int
+fun add(num1: Int, num2: Int):Int{
+    val result = num1 + num2
+    return result
+}
